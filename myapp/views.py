065a34abe779d135985816ada2f1fcfb3a363abe
musafir1banjaara <kunal.work786@gmail.com>
2026-07-25 01:45:26 +0530

    Response from view.py using HttpResponse

--- myapp/views.py
+++ myapp/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Hello Django")
+
+
+def about(request):
+    return HttpResponse("This is About Page")
