1c126a9373d4fc167ae890f25f9564e349f73e26
musafir1banjaara <kunal.work786@gmail.com>
2026-07-26 01:04:24 +0530

    Created first table in models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    salary = models.IntegerField()
+
+
+
+# Dunder methods (short for "double underscore" methods), also known as magic methods, are special built-in Python functions that start and end with two underscores
+    def __str__(self):
+        return self.company_name
+
+# Returns a precise, developer-friendly string representation
