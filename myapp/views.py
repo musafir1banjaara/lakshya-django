3b7938abfa797a814cbe29d1d6e479d4398fea5c
musafir1banjaara <kunal.work786@gmail.com>
2026-07-26 01:46:41 +0530

    Passing, Accessing, Looping data from views to template

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
+jobs = [
+       { "title" : "Frontend Developer", 
+         "company" : "Google", 
+         "location" : "Remote", 
+         "salary" : "$120k" 
+       },
+       { "title" : "Product Designer",
+         "company" : "Figma", 
+         "location" : "San Francisco, CA", 
+         "salary" : "$135k" 
+        }
+        ]
+
 # Create your views here.
 def home(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {"jobs":jobs})
 
 
 def about(request):
